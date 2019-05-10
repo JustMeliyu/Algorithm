d0c268f3b14f34cc0f44f7cfd42d05d9d72e8a8b
JustMeliyu <642464509@qq.com>
2019-05-10 18:03:24 +0800

    add sudoku

--- Sudoku.py
+++ Sudoku.py
@@ -15,24 +15,67 @@ Describe:
 """
 
 
-def solve_sudoku(board) -> None:
-    """
-    Do not return anything, modify board in-place instead.
-    """
-    for i in range(9):
-        a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        for j in range(9):
-            if board[i][j] == ".":
-                pass
-            else:
-                a.remove(board[i][j])
+class Solution:
+    def __init__(self):
+        self.remaining = {}
+        self.pro_index = [(0, 0)]
+        for i in range(9):
+            for j in range(9):
+                self.remaining[(i, j)] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    def solve_sudoku(self, board) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        for i in range(9):
+            for j in range(9):
+                b = board[i]
+                if board[i][j] == ".":
+                    while self.remaining[(i, j)]:
+                        if self.remaining[(i, j)][0] not in board[i]:
+                            board[i][j] = self.remaining[(i, j)][0]
+                            if self.judge_repeat(board, i, j):
+                                self.pro_index.append((i, j))
+                                break
+                        del self.remaining[(i, j)][0]
+                    else:
+                        self.remaining[(i, j)] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                        while not self.remaining[self.pro_index[-1]]:
+                            board[self.pro_index[-1][0]][self.pro_index[-1][1]] = "."
+                            del self.pro_index[-1]
+
+                        del self.remaining[self.pro_index[-1]][0]
+                        board[i][j] = "."
+                        board[self.pro_index[-1][0]][self.pro_index[-1][1]] = "."
+                        del self.pro_index[-1]
+                        self.solve_sudoku(board)
+                        break
+
+    @classmethod
+    def judge_repeat(cls, board: list, i: int, j: int):
+        tmp = []
+        for q in range(9):
+            if board[q][j] in tmp:
+                return False
+            if board[q][j] != ".":
+                tmp.append(board[q][j])
+        a = i // 3
+        b = j // 3
+        tmp2 = []
+        for m in range(a * 3, a * 3 + 3):
+            for n in range(b * 3, b * 3 + 3):
+                if board[m][n] in tmp2:
+                    return False
+                if board[m][n] != ".":
+                    tmp2.append(board[m][n])
+        return True
 
 
 if __name__ == '__main__':
     sudoku = [
         [5, 3, ".", ".", 7, ".", ".", ".", "."],
         [6, ".", ".", 1, 9, 5, ".", ".", "."],
-        [".", 9, 8, "", ".", ".", ".", 6, "."],
+        [".", 9, 8, ".", ".", ".", ".", 6, "."],
         [8, ".", ".", ".", 6, ".", ".", ".", 3],
         [4, ".", ".", 8, ".", 3, ".", ".", 1],
         [7, ".", ".", ".", 2, ".", ".", ".", 6],
@@ -40,4 +83,9 @@ if __name__ == '__main__':
         [".", ".", ".", 4, 1, 9, ".", ".", 5],
         [".", ".", ".", ".", 8, ".", ".", 7, 9]
     ]
-    solve_sudoku(sudoku)
+    s = Solution()
+    s.solve_sudoku(sudoku)
+    print(sudoku)
+
+    # v = {(1, 3): "10000000000"}
+    # print(v[(1, 3)])
