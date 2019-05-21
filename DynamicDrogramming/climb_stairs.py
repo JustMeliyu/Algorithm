4d096d89ae9475b7d555dbd4b244f75f2c5512ca
JustMeliyu <642464509@qq.com>
2019-05-21 18:00:19 +0800

    modify climb stairs

--- DynamicDrogramming/climb_stairs.py
+++ DynamicDrogramming/climb_stairs.py
@@ -7,7 +7,9 @@ Describe: 爬楼梯
 一共有10级楼梯, 每次只能走1步或者2步, 爬上这个楼梯一共需要多少种方式
 """
 import sys
-from tools.common import get_func_time, run_method
+from tools.common import run_method
+from tools.logger import logger
+from functools import lru_cache
 sys.setrecursionlimit(10000)    # 增加递归次数
 
 
@@ -53,29 +55,41 @@ class Solution(object):
         :param step:
         :return:
         """
+
+        if step == 1:
+            return 1
+        if step == 2:
+            return 2
         a = 1
         b = 2
-        if step == 1:
-            return a
-        if step == 2:
-            return b
-        t = 3
-        while t <= step:
+        for i in range(step - 2):
             self.count += 1
             b, a = a + b, b
-            t += 1
         return b
+
+    @lru_cache(10**8)
+    def climb_stairs4(self, step):
+        self.count += 1
+        if step == 1:
+            return 1
+        elif step == 2:
+            return 2
+        else:
+            return self.climb_stairs4(step - 1) + self.climb_stairs4(step - 2)
 
 
 if __name__ == '__main__':
     info = {}
     s = Solution()
     run_method(s.climb_stairs, 20)
-    print(s.count)
+    logger.info(s.count)
     s.count = 0
     run_method(s.climb_stairs2, 1000, info)
-    print(s.count)
+    logger.info(s.count)
     s.count = 0
     run_method(s.climb_stairs3, 1000)
-    print(s.count)
+    logger.info(s.count)
+    s.count = 0
+    run_method(s.climb_stairs4, 1000)
+    logger.info(s.count)
     s.count = 0
