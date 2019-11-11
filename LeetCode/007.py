ada3827ee05524945b3e1dd025ec17f1469eded5
JustMeliyu <642464509@qq.com>
2019-11-11 10:30:14 +0800

    #整数反转

--- LeetCode/007.py
+++ LeetCode/007.py
@@ -22,3 +22,24 @@
 来源：力扣（LeetCode）
 链接：https://leetcode-cn.com/problems/reverse-integer
 """
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if -2 ** 31 > x or x > (2 ** 31 - 1):
+            return 0
+
+        def _reverse(_x: int) -> int:
+            a = int(str(_x)[::-1])
+            return 0 if a < -2 ** 31 or a > 2 ** 31 - 1 else a
+
+        if x < 0:
+            return -_reverse(-x)
+        else:
+            return _reverse(x)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.reverse(1534236469))
+    print(2 ** 31)
