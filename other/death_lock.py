cd578cae682e410630912c96b82faf42cf5670b8
justme64 <642464509@qq.com>
2020-03-30 16:57:00 +0800

    0330-死锁

--- other/death_lock.py
+++ other/death_lock.py
@@ -8,9 +8,9 @@ Describe:
 在多线程中，死锁的线程，多半是因为一个线程需要获取多个锁造成的
 """
 
-# coding=utf-8
 import time
 import threading
+from contextlib import contextmanager
 
 
 class Account:
@@ -29,13 +29,29 @@ class Account:
 
 
 def transfer(_from, to, amount):
+    # 可能会形成死锁
     if _from.lock.acquire():  # 锁住自己的账户
         _from.withdraw(amount)
         time.sleep(1)  # 让交易时间变长，2个交易线程时间上重叠，有足够时间来产生死锁
         print('wait for lock...')
-        if to.lock.acquire(timeout=1):  # 锁住对方的账户
+        if to.lock.acquire():  # 锁住对方的账户
             to.deposit(amount)
             to.lock.release()
+        _from.lock.release()
+    print('finish...')
+
+
+def transfer2(_from, to, amount):
+    if _from.lock.acquire():  # 锁住自己的账户
+        _from.withdraw(amount)
+        time.sleep(1)  # 让交易时间变长，2个交易线程时间上重叠，有足够时间来产生死锁
+        print('wait for lock...')
+        if to.lock.acquire(timeout=1):  # 锁住对方的账户，超时
+            to.deposit(amount)
+            to.lock.release()
+        else:
+            print('timeout\n')  # 放置死锁
+            _from.deposit(amount)
         _from.lock.release()
     print('finish...')
 
@@ -43,8 +59,12 @@ def transfer(_from, to, amount):
 if __name__ == '__main__':
     a = Account('a', 1000, threading.Lock())
     b = Account('b', 1000, threading.Lock())
+    # a = Account('a', 1000, threading.RLock())
+    # b = Account('b', 1000, threading.RLock())
     threading.Thread(target=transfer, args=(a, b, 100)).start()
     threading.Thread(target=transfer, args=(b, a, 200)).start()
+    threading.Thread(target=transfer2, args=(a, b, 100)).start()
+    threading.Thread(target=transfer2, args=(b, a, 200)).start()
     time.sleep(3)
     print(a.balance)
     print(b.balance)
